9725298ef6730e996545968d565f3ac0807fd6ec
andreykravts <andreykravts2210@gmail.com>
2024-01-08 16:31:26 +0200

    First lessons creating of simple consumer and json simple consumer

---
+++ src/test/java/dev/lydtech/dispatch/service/DispatchServiceTest.java
@@ -0,0 +1,25 @@
+package dev.lydtech.dispatch.service;
+
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.util.TestEventData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static java.util.UUID.randomUUID;
+
+class DispatchServiceTest {
+
+    private  DispatchService service;
+
+    @BeforeEach
+    void setUp() {
+        service=new DispatchService();
+    }
+
+    @Test
+    void process() {
+        OrderCreated testEvent = TestEventData.builderOrderCreatedEvent(randomUUID(),randomUUID().toString());
+        service.process(testEvent);
+
+    }
+}
